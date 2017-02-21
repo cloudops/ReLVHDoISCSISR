4bafe23081b36fda6cb65eec577bebe3bd7f12a0
Syed <syed1.mushtaq@gmail.com>
2017-02-20 21:01:39 -0500

    Add install script

---
+++ install_iso.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Usage: install_iso.sh <xenserver_ip> <root password>
+
+scp *iso root@$1:
+
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 mkdir -p /tmp/test
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 rm -f *.iso
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 wget -c 'https://github.com/cloudops/ReLVHDoISCSISR/raw/master/ReLVHDoISCSISR.iso'  --no-check-certificate
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 mount ReLVHDoISCSISR.iso /tmp/test -o loop
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 rpm -ivh /tmp/test/ReLVHDoISCSISR-1.0-1.x86_64.rpm --force
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 umount /tmp/test
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 rmdir /tmp/test
+sshpass -p$2 ssh -o StrictHostKeyChecking=no root@$1 service xapi restart
+
